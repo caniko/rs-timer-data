f3e472112deb83ec051a3e93f34237c1b75bedfa
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-06-14 23:34:49 +0200

    Add managed Pages project site

--- flake.nix
+++ flake.nix
@@ -5,14 +5,34 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plinth = {
+      url = "git+https://codeberg.org/caniko/plinth.git?ref=refs/heads/trunk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, plinth }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
+      packages = forAllSystems (system:
+        let
+          website = plinth.lib.${system}.mkProjectSite {
+            pname = "timer-data-website";
+            domain = "timer-data.tartanoglu.com";
+            configPath = ./website/plinth-project.toml;
+          };
+        in {
+          inherit website;
+          site = website;
+        });
+      apps = forAllSystems (system: {
+        deploy-pages = plinth.lib.${system}.mkDeployPagesApp {
+          domain = "timer-data.tartanoglu.com";
+        };
+      });
       devShells = forAllSystems (system:
         let
           pkgs = import nixpkgs {
